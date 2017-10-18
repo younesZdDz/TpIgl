b96cb9059fe3ab1a9a28a66039a28885916613f3
omarSek <fo_ouaissi_sekouti@esi.dz>
2017-10-18 08:59:14 +0100

    ajout classe exception qui gere le cas ou deux table de taille differente dans la fonction de somme de tableau

--- src/utilVecteur/VectorHelper.java
+++ src/utilVecteur/VectorHelper.java
@@ -1,14 +1,4 @@
 package utilVecteur;
 class VectorHelper {
-public static int [] somme (int tab1[],int tab2[])throws exceptionTailleDiff{
-		
-		if (tab1.length != tab2.length) throw new exceptionTailleDiff() ;
-		else {
-			int tabResul[]= new int [tab1.length];
-			int i =0 ; 
-			for ( i=0 ;i < tab1.length ;i++){
-				tabResul[i]=tab1[i]+tab2[i];
-			}
-			return tabResul;} 
-	}
+
 }
