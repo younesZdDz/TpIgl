063ed821522298a2a61641596df4e19d204eae7e
younesZdDz <32100131+younesZdDz@users.noreply.github.com>
2017-10-18 11:20:48 +0200

    ajout de documentaion

--- src/utilVecteur/TableauVideException.java
+++ src/utilVecteur/TableauVideException.java
@@ -1,5 +1,10 @@
 package utilVecteur;
-
+/**
+ * Classe d'exception pour le cas ou utilisateur oublie d'inialiser le tableau
+ * 
+ * @author Younes Zadi
+ *
+ */
 class TableauVideException extends Exception{
 
 }
--- src/utilVecteur/VectorHelper.java
+++ src/utilVecteur/VectorHelper.java
@@ -1,5 +1,10 @@
 package utilVecteur;
-
+/**
+ * Classe utilitaire pour les traitement des vecteurs de type entier.
+ *  
+ * @author Younes Zadi
+ *
+ */
 class VectorHelper {// test de conflit de tp igl 
 
 public static int [] somme (int tab1[],int tab2[])throws exceptionTailleDiff{
@@ -24,6 +29,22 @@ public static int [] somme (int tab1[],int tab2[])throws exceptionTailleDiff{
 			}
 	}
 	// fonction 
+	/**
+	 * Fonction pour le calcul du maximum et minimum simultanément d'un 
+	 * {@code tableau} d'entier.
+	 *  
+	 * @param tableau le {@code tableau} a calculé son maximum et minimum
+	 * @return  {@code tableau} de deux case la premiere est le min, et la seconde 
+	 *	est le max. 
+	 * @throws TableauVideException
+	 * <br>
+	 * <b>Exemple</b>
+	 * <br>
+	 * <pre>
+	 * Si on donne comme entree le tableau {2,3,8,9,15,11}
+	 * on aura comme sortie {2,15}
+	 * </pre>
+	 */
 	public static int [] minMaxTab(int tableau[])throws TableauVideException{
 		if (tableau==null){
 			throw new TableauVideException();
@@ -45,7 +66,20 @@ public static int [] somme (int tab1[],int tab2[])throws exceptionTailleDiff{
 			return tabTemp;
 		}
 	}
-
+	/**
+	 * Procedure pour faire une operation de produit de tous les case d'un 
+	 * {@code tableau} par un entier donnee.
+	 * 
+	 * @param tableau le {@code tableau} dont on veut traiter.
+	 * @param n l'entier a multipilié par tous les case du {@code tableau}
+	 * <br>
+	 * <b>Exemple</b>
+	 * <br>
+	 * <pre>
+	 * si on donne comme entree {2,8,9,11}, et l'entier n=2 le tableau devient
+	 * {4,16,18,22}
+	 * </pre>
+	 */
 	public static void mulFoisNTableau(int tableau[],int n){
 		for (int i=0;i<tableau.length;i++)
 			tableau[i]=tableau[i]*2;
